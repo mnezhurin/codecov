79e3de66839d9aa53d162a464c79498329c901b7
Maksym Nezhurin <maksym.nezhurin@wework.com>
2022-01-20 12:47:21 +0200

    Add react and test suits

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Link from "./components/Link";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Link page="https://reactjs.org" classNames="App-link" children="New React Link"/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
